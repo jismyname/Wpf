48c377c48e3687d5fd3091ccd7b7a3585829d237
jismyname <124774183+jismyname@users.noreply.github.com>
2023-11-17 07:16:45 +0800

    Update ExportExcleByOpenXml.cs

--- ExportExcleByOpenXml.cs
+++ ExportExcleByOpenXml.cs
@@ -1,3 +1,4 @@
+using ClosedXML.Excel;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Drawing.Charts;
 using DocumentFormat.OpenXml.Packaging;
@@ -7,6 +8,7 @@ using System.Data;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
+using System.Xml;
 
 namespace ConsoleApp2
 {
@@ -14,132 +16,67 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            string file = "a.xlsx";
-            var sheetdoc = SpreadsheetDocument.Create(file, DocumentFormat.OpenXml.SpreadsheetDocumentType.Workbook);
+            var wb = new XLWorkbook();
+            var ws = wb.Worksheets.Add("Using Colors");
 
-            var workbookpart = sheetdoc.AddWorkbookPart();
-            workbookpart.Workbook = new();
+            Int32 ro = 0;
 
-            var worksheetpart = workbookpart.AddNewPart<WorksheetPart>();
-            worksheetpart.Worksheet = new Worksheet(new SheetData());
+            // From Known color
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.Red;
+            ws.Cell(ro, 2).Value = "XLColor.Red";
+
+            // From Color not so known
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.Byzantine;
+            ws.Cell(ro, 2).Value = "XLColor.Byzantine";
+
+            ro++;
+
+            // FromArgb(Int32 argb) using Hex notation
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(0xFF00FF);
+            ws.Cell(ro, 2).Value = "XLColor.FromArgb(0xFF00FF)";
+
+            // FromArgb(Int32 argb) using an integer (you need to convert the hex value to an int)
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(16711935);
+            ws.Cell(ro, 2).Value = "XLColor.FromArgb(16711935)";
+
+            // FromArgb(Int32 r, Int32 g, Int32 b)
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(255, 0, 255);
+            ws.Cell(ro, 2).Value = "XLColor.FromArgb(255, 0, 255)";
+
+            // FromArgb(Int32 a, Int32 r, Int32 g, Int32 b)
+            // Note: Excel ignores the alpha value
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(0, 255, 0, 255);
+            ws.Cell(ro, 2).Value = "XLColor.FromArgb(0, 255, 0, 255)";
+
+            ro++;
 
 
-            workbookpart.AddNewPart<WorkbookStylesPart>();
-            workbookpart.WorkbookStylesPart.Stylesheet = new();
-            var styleSheet = workbookpart.WorkbookStylesPart.Stylesheet;
-            List<UInt32> indexRef = addheaderstyle(ref styleSheet);
-            styleSheet.Save();
+            // FromHtml(String htmlColor)
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromHtml("#FF996515");
+            ws.Cell(ro, 2).Value = "XLColor.FromHtml(\"#FF996515\")";
 
-            var sheets = sheetdoc.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
+            ro++;
 
-            var sheet = new Sheet()
-            {
-                Id = sheetdoc.WorkbookPart.GetIdOfPart(worksheetpart),
-                SheetId = 1,
-                Name = "sheet 1"
-            };
+            // FromIndex(Int32 indexedColor)
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromIndex(25);
+            ws.Cell(ro, 2).Value = "XLColor.FromIndex(25)";
 
-            sheets.Append(sheet);
-            var worksheet = worksheetpart.Worksheet;
+            ro++;
 
-            var sheetdata = worksheet.GetFirstChild<SheetData>();
+            // From Theme color
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromTheme(XLThemeColor.Accent1);
+            ws.Cell(ro, 2).Value = "XLColor.FromTheme(XLThemeColor.Accent1)";
 
-            Merge(worksheet);
+            // From Theme color with tint
+            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromTheme(XLThemeColor.Accent1, 0.5);
+            ws.Cell(ro, 2).Value = "XLColor.FromTheme(XLThemeColor.Accent1, 0.5)";
 
-            var r = new Row();
-            var cell = new Cell()
-            {
-                CellValue = new CellValue("j developer"),
-                DataType = CellValues.String,
-                CellReference="A1"
-            };
-            r.AppendChild(cell);
-            cell.StyleIndex = indexRef[2];
-           
+            ws.Columns().AdjustToContents();
 
-            sheetdata.Append(r);
-
-            ExportData(sheetdata);
-
-            workbookpart.Workbook.Save();
-            sheetdoc.Dispose();
+            wb.SaveAs("UsingColors.xlsx");
             Console.WriteLine("done");
-
         }
 
-        private static void Merge(Worksheet worksheet)
-        {
-            MergeCells mergeCells = new();
-            worksheet.InsertAfter(mergeCells, worksheet.Elements<SheetData>().First());
-
-            var mergecell = new MergeCell() { Reference = new StringValue("A1:B1") };
-            mergeCells.Append(mergecell);
-            worksheet.Save();
-        }
-
-        private static void ExportData(SheetData sheetdata)
-        {
-            var dt = new System.Data.DataTable();
-            dt.Columns.Add("a");
-            dt.Columns.Add("b");
-            dt.Columns.Add("c");
-            dt.Rows.Add(1, 2, 3);
-            dt.Rows.Add(4, 5, 6);
-            UInt32Value rowindex = 2;
-
-            foreach (DataRow item in dt.Rows)
-            {
-                var row = new Row() { RowIndex = rowindex };
-                for (int i = 0; i < item.ItemArray.Count(); i++)
-                {
-                    var c = new Cell()
-                    {
-                        CellValue = new CellValue(item[i].ToString()),
-                        DataType = CellValues.String
-                    };
-                    row.Append(c);
-                }
-                sheetdata.Append(row);
-                rowindex++;
-            }
-
-
-        }
-
-        private static List<uint> addheaderstyle(ref Stylesheet styleSheet)
-        {
-            UInt32 fontid = 0, fillid = 0, cellformatid = 0;
-
-            Font font = new Font(new FontSize() { Val = 15 },
-                new Color() { Rgb = HexBinaryValue.FromString("252525") });
-            styleSheet.Fonts = new();
-            styleSheet.Fonts.AppendChild(font);
-            styleSheet.Fonts.Count = new() { Value = 0 };
-            fontid = styleSheet.Fonts.Count++;
-
-            var patternfill = new PatternFill() { PatternType = PatternValues.Solid };
-            patternfill.ForegroundColor = new ForegroundColor() { Rgb = HexBinaryValue.FromString("252525") };
-
-            styleSheet.Fills = new();
-            styleSheet.Fills.Append(new Fill() { PatternFill = patternfill });
-            styleSheet.Fills.Count = new() { Value = 0 };
-            fillid = styleSheet.Fills.Count++;
-
-            var cellformat = new CellFormat()
-            {
-                FontId = fontid,
-                FillId = fillid,
-                ApplyFill = true,
-                Alignment = new Alignment() { Horizontal = HorizontalAlignmentValues.Center }
-            };
-
-            styleSheet.CellFormats = new();
-            styleSheet.CellFormats.AppendChild(cellformat);
-            styleSheet.CellFormats.Count = new() { Value = 0 };
-            cellformatid = styleSheet.CellFormats.Count++;
-
-            return new List<uint> { fontid, fillid, cellformatid };
-        }
     }
 
 
