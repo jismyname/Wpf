6a44a35c8f507a430371f6838f25eacb38247e03
jismyname <124774183+jismyname@users.noreply.github.com>
2023-11-17 11:37:15 +0800

    Update ExportExcleByOpenXml.cs

--- ExportExcleByOpenXml.cs
+++ ExportExcleByOpenXml.cs
@@ -1,84 +1,105 @@
 using ClosedXML.Excel;
-using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Drawing.Charts;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Spreadsheet;
-using DocumentFormat.OpenXml.Vml.Office;
 using System.Data;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Xml;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            var wb = new XLWorkbook();
-            var ws = wb.Worksheets.Add("Using Colors");
+            var data = new DataTable();
+            data.Columns.Add("a");
+            data.Columns.Add("b");
+            data.Columns.Add("c");
+            data.Columns.Add("cc");
+            data.Columns.Add("cd");
+            data.Rows.Add(1, 2, 3, 33, 44);
+            data.Rows.Add(4, 5, 6, 55, 66);
 
-            Int32 ro = 0;
-
-            // From Known color
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.Red;
-            ws.Cell(ro, 2).Value = "XLColor.Red";
-
-            // From Color not so known
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.Byzantine;
-            ws.Cell(ro, 2).Value = "XLColor.Byzantine";
-
-            ro++;
-
-            // FromArgb(Int32 argb) using Hex notation
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(0xFF00FF);
-            ws.Cell(ro, 2).Value = "XLColor.FromArgb(0xFF00FF)";
-
-            // FromArgb(Int32 argb) using an integer (you need to convert the hex value to an int)
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(16711935);
-            ws.Cell(ro, 2).Value = "XLColor.FromArgb(16711935)";
-
-            // FromArgb(Int32 r, Int32 g, Int32 b)
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(255, 0, 255);
-            ws.Cell(ro, 2).Value = "XLColor.FromArgb(255, 0, 255)";
-
-            // FromArgb(Int32 a, Int32 r, Int32 g, Int32 b)
-            // Note: Excel ignores the alpha value
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromArgb(0, 255, 0, 255);
-            ws.Cell(ro, 2).Value = "XLColor.FromArgb(0, 255, 0, 255)";
-
-            ro++;
+            var headers = new List<ExcelHeaderItem>();
+            headers.Add(new ExcelHeaderItem() { HeaderName = "id",ColumnSpan=2 });
+            headers.Add(new ExcelHeaderItem() { HeaderName = "datas", ColumnSpan = 2 });
+            headers.Add(new ExcelHeaderItem() { HeaderName = "new column", ColumnSpan = 2, RowSpan = 2 });
 
 
-            // FromHtml(String htmlColor)
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromHtml("#FF996515");
-            ws.Cell(ro, 2).Value = "XLColor.FromHtml(\"#FF996515\")";
-
-            ro++;
-
-            // FromIndex(Int32 indexedColor)
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromIndex(25);
-            ws.Cell(ro, 2).Value = "XLColor.FromIndex(25)";
-
-            ro++;
-
-            // From Theme color
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromTheme(XLThemeColor.Accent1);
-            ws.Cell(ro, 2).Value = "XLColor.FromTheme(XLThemeColor.Accent1)";
-
-            // From Theme color with tint
-            ws.Cell(++ro, 1).Style.Fill.BackgroundColor = XLColor.FromTheme(XLThemeColor.Accent1, 0.5);
-            ws.Cell(ro, 2).Value = "XLColor.FromTheme(XLThemeColor.Accent1, 0.5)";
-
-            ws.Columns().AdjustToContents();
-
-            wb.SaveAs("UsingColors.xlsx");
+            ExportToExcel(data, "a.xlsx", headers);
             Console.WriteLine("done");
         }
 
+        private static void ExportToExcel(DataTable dataTable, string fileName, List<ExcelHeaderItem> excelHeaderItems)
+        {
+            if (dataTable == null)
+            {
+                return;
+            }
+
+            int startRow = 1;
+            using (XLWorkbook workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("sheet");
+
+                // write excel header
+                if (excelHeaderItems == null || excelHeaderItems.Count == 0)
+                {
+                    for (int i = 0; i < dataTable.Columns.Count; i++)
+                    {
+                        worksheet.Cell(1, i + 1).Value = dataTable.Columns[i].ColumnName;
+                        worksheet.Cell(1, i + 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    }
+                }
+                else
+                {
+                    startRow = excelHeaderItems.Max(x => x.RowSpan);
+                    int columnIndex = 0;
+                    foreach (var item in excelHeaderItems)
+                    {
+                        if (item.RowSpan > 1 || item.ColumnSpan > 1)
+                        {
+                            worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1 + columnIndex).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                            worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1 + columnIndex).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                            worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1+ columnIndex).Value = item.HeaderName;
+                            worksheet.Range(worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1 + columnIndex), worksheet.Cell(item.RowSpan, excelHeaderItems.IndexOf(item) + columnIndex+item.ColumnSpan)).Merge();
+                            columnIndex += item.ColumnSpan-1;
+                        }
+                        else
+                        {
+                            worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1).Value = item.HeaderName;
+                            worksheet.Cell(1, excelHeaderItems.IndexOf(item) + 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                        }
+                        worksheet.Cell(1, 1).Style.Fill.BackgroundColor = XLColor.FromHtml("#FF996515");
+                        worksheet.Cell(1, 3).Style.Fill.BackgroundColor = XLColor.FromArgb(0xFF00FF);
+                    }
+                }
+
+                startRow++;
+                for (int i = 0; i < dataTable.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataTable.Columns.Count; j++)
+                    {
+                        worksheet.Cell(startRow + i, j + 1).Value = dataTable.Rows[i][j].ToString();
+                        worksheet.Cell(startRow + i, j + 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    }
+                }
+
+                workbook.SaveAs(fileName);
+            }
+        }
+
+
+        private static void DataTableReaderDemo()
+        {
+            var dt = new DataTable();
+            dt.Columns.Add("Name");
+            dt.Columns.Add("city");
+            dt.Rows.Add("zhag", "zunyi");
+            dt.Rows.Add("wang", "dd");
+            var reader = dt.CreateDataReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+                Console.WriteLine(reader.GetString(1));
+            }
+        }
     }
-
-
-
 }
